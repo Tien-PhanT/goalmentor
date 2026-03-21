b5844a66aaeb7987ab468d80d07f1c77ee657a7f
Tien-PhanT <112714509+phtanya@users.noreply.github.com>
2026-03-21 16:10:08 +0100

    feat: add validation and global exception handler

--- user-service/src/main/java/com/goalmentor/userservice/dto/UserRequest.java
+++ user-service/src/main/java/com/goalmentor/userservice/dto/UserRequest.java
@@ -1,11 +1,24 @@
 package com.goalmentor.userservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserRequest {
+
+    @NotBlank(message = "Email is required!")
+    @Email(message = "Email is invalid")
     private String email;
+
+    @NotBlank(message = "Username is required!")
+    @Size(min = 3, max= 50, message = "Username must be between 3 and 50 characters")
     private String username;
+
+    @NotBlank(message = "Password is required!")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
+
     private String fullName;
 }
