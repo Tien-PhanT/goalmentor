95b7d7f8b6cb8f80b7ce5bfa537011708cb7911c
Tien-PhanT <112714509+phtanya@users.noreply.github.com>
2026-03-22 10:36:56 +0100

    feat: add update and delete methods with custom exception and unit tests

--- user-service/src/main/java/com/goalmentor/userservice/controller/UserController.java
+++ user-service/src/main/java/com/goalmentor/userservice/controller/UserController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,5 +36,17 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<UserResponse>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id,
+                                                   @Valid @RequestBody UserRequest request) {
+        return ResponseEntity.ok(userService.updateUser(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- user-service/src/main/java/com/goalmentor/userservice/service/UserService.java
+++ user-service/src/main/java/com/goalmentor/userservice/service/UserService.java
@@ -3,6 +3,7 @@ package com.goalmentor.userservice.service;
 import com.goalmentor.userservice.dto.UserRequest;
 import com.goalmentor.userservice.dto.UserResponse;
 import com.goalmentor.userservice.entity.User;
+import com.goalmentor.userservice.exception.UserNotFoundException;
 import com.goalmentor.userservice.mapper.UserMapper;
 import com.goalmentor.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -29,7 +30,7 @@ public class UserService {
 
     public UserResponse getUserById(Long id) {
         User user = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new UserNotFoundException(id));;
         return userMapper.toResponse(user);
     }
 
@@ -39,4 +40,25 @@ public class UserService {
                 .map(userMapper::toResponse)
                 .collect(Collectors.toList());
     }
+
+    public UserResponse updateUser(Long id, UserRequest request) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setEmail(request.getEmail());
+        user.setFullName(request.getFullName());
+
+        User saved = userRepository.save(user);
+        return userMapper.toResponse(saved);
+    }
+
+    public void deleteUser(Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+    }
+
+
 }
